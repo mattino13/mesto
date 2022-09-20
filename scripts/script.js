d1b21aa7ecf88bc85ad6b3df3f3698446158a182
mattino <mattino13@yandex.ru>
2022-09-20 23:39:23 +0300

    hotfix/PR1

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,44 @@
-function togglePopup (){
-  const Popup = document.querySelector('.popup');
-  Popup.classList.toggle('popup__popup_opened');
-}
+// Находим Popup
+const popup = document.querySelector('.popup');
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
+const formElement = document.querySelector('.popup__form');
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('#nameInput');
-let jobInput = formElement.querySelector('#jobInput');
+const nameInput = formElement.querySelector('.popup__item_input_name');
+const jobInput = formElement.querySelector('.popup__item_input_job');
+
+// Ищем элементы, в которые должны быть вставлены значения полей
+const profileTittle = document.querySelector('.profile__tittle');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+function openPopup() {
+  nameInput.value = profileTittle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    evt.preventDefault(); 
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-    const profileTittle = document.querySelector('.profile__tittle');
-    const profileSubtitle = document.querySelector('.profile__subtitle');
-
-    // Вставьте новые значения с помощью textContent
+    // Сохранение новых значений с помощью textContent
     profileTittle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
 
-    togglePopup ();
+    closePopup ();
 }
 
 const openPopupButton = document.querySelector('.profile__edit-button');
-openPopupButton.addEventListener('click', togglePopup);
+openPopupButton.addEventListener('click', openPopup);
 
 const closePopupButton = document.querySelector('.popup__button-close');
-closePopupButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', closePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
